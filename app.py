34d34e9833d086d8db6f2ddbeef959dd8ff745c2
John Rodolph Bacalso <111875334+johnrodolph@users.noreply.github.com>
2024-09-04 10:07:36 +0800

    updated my hobbies list

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ with tab2:
 with tab3:
     st.balloons()
     st.title("Hobbies")
-    items = ["I play sports such as Basketball and Volleyball", "I also love playing Online Games", "Draw anime characters ", "I also spend my free time practing new programming languages"]
+    items = ["Enjoy playing sports like basketball and volleyball.", "Passionate about online gaming.", "Love drawing anime characters in my spare time. ", "Dedicate some of my free time to practicing new programming languages."]
     markdown_text = "\n".join([f"- {item}" for item in items])
     st.markdown(markdown_text)
 
